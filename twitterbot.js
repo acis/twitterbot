f4dae4c14afd2bec4275f5c02af630642151407e
Anca Cismasiu <anca.cismasiu@meinkauf.at>
2013-11-01 23:05:04 +0100

    parse json responses into individual tweets

--- twitterbot.js
+++ twitterbot.js
@@ -1,44 +1,71 @@
-var https = require('https'),
-	OAuth = require('oauth'),
-	keys = require('./keys.js');
+var https = require('https');
+var	qs = require('qs');
+var	request = require('request');
+var	JSONStream = require('JSONStream');
+var	OAuth = require('oauth');
+var	neo4j= require('neo4j');
+var	streamline = require('streamline');
+var	es = require('event-stream');
+var	keys = require('./keys.js');
+
+var neodb = new neo4j.GraphDatabase('http://localhost:7474');
+var stream = JSONStream.parse();
+var bearerToken;
 
 
-//get bearer token
-var OAuth2 = OAuth.OAuth2;
+function getBearerToken(){
+	var OAuth2 = OAuth.OAuth2;
+	var oauth2 = new OAuth2(keys.consumer_key, keys.consumer_secret, 'https://api.twitter.com/', null, 'oauth2/token', null);
+	oauth2.getOAuthAccessToken('', {'grant_type': 'client_credentials'}, function(e, access_token, refresh_token, results) {
+				bearerToken = access_token;
+				getTweets();
+			}
+	);
+}
 
-var oauth2 = new OAuth2(keys.consumer_key,keys.consumer_secret,'https://api.twitter.com/', null, 'oauth2/token', null);
+function getTweets(){
 
-oauth2.getOAuthAccessToken('',{'grant_type':'client_credentials'},function (e, access_token, refresh_token, results){
-
-        makeRequest(access_token);
-		}
-);
-
-
-function makeRequest(bearer_token){
-
-    var options = {
-        hostname: 'api.twitter.com',
-        port:443,
-        //path:"/1.1/statuses/user_timeline.json?screen_name=twitterapi&count=2",
-        path: '/1.1/search/tweets.json?q=%23freebandnames&since_id=24012619984051000&max_id=250126199840518145&result_type=mixed&count=4',
-        method:"GET",
-        headers: {
-            'User-Agent': 'AIC Data Mining',
-            Authorization: "Bearer "+bearer_token
-        }
-    };
-
-      var req = https.request(options, function(res) {
-        res.on('data', function(d) {
-            process.stdout.write(d);
-        });
-    });
-    req.end();
-
-    req.on('error', function(e) {
-        console.error(e);
-    });
+	 ///think up a smart algorithm for traversing tweets and make requests
+	var path='/1.1/statuses/user_timeline.json?';
+	var params={screen_name:'twitterapi'};
+	makeRequest(path, params);
 
 }
+
+
+
+function makeRequest(path, params) {
+
+	var options ={
+		url: "https://api.twitter.com"+path,
+		qs: params,
+		headers: {
+			'User-Agent': 'AIC Data Mining',
+			Authorization: "Bearer " + bearerToken
+		}
+	};
+
+	request(options).pipe(stream);
+	stream.on('root', function(obj){
+		obj.forEach(function(entry) {
+			//SAVE DATA
+		/*	var node = db.createNode({hello: 'world'});
+			node.save(function (err, node) {
+				if (err) {
+					console.err('Error saving new node to database:', err);
+				} else {
+					console.log('Node saved to database with id:', node.id);
+				}
+			}); */
+		});
+
+
+
+
+
+	})
+
+}
+
+getBearerToken();
 
